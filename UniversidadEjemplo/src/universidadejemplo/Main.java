147162afb42a51e36f3a38c51a7f2b9918e33d85
Ezequiel Hermoso <108770968+Ezequiel7100@users.noreply.github.com>
2024-05-16 10:30:56 -0300

    pruebas del main

--- UniversidadEjemplo/src/universidadejemplo/Main.java
+++ UniversidadEjemplo/src/universidadejemplo/Main.java
@@ -3,14 +3,29 @@ package universidadejemplo;
 import AccesoADatos.AlumnoData;
 import Entidades.Alumno;
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         LocalDate fecha = LocalDate.of(1999, 2, 10);
-        Alumno alumno=new Alumno(134,"pablo","gomez",fecha,true);
+        Alumno alumno=new Alumno(13423232,"pablo","gomez",fecha,true);
+        Alumno alumnoModifi=new Alumno(10,12345,"Lucas","Perez",fecha,true);
         AlumnoData ad = new AlumnoData();
-        ad.guardarAlumno(alumno);
+        //ad.guardarAlumno(alumno);
+        
+        //Alumno alumno2 = ad.buscarAlumno(10);
+        //System.out.println(alumno2);
+        
+        //List<Alumno> alumnos = ad.listarAlumnos();
+        //System.out.println(alumnos);
+        
+        
+       // ad.modificarAlumno(alumnoModifi);
+        
+        
+        ad.eliminarAlumno(10);
+        
     }
     
 }
